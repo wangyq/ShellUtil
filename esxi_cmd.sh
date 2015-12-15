03ce8fdf52c9266ec60c5a2bd8c68a9c38b0d3d2
Yinqing Wang <yinqingwang@gmail.com>
2015-12-15 18:48:22 +0800

    modify the use of array

--- esxi_cmd.sh
+++ esxi_cmd.sh
@@ -10,10 +10,11 @@ SW=vSwitch3
 
 i=15
 #LAN=(R1-R2 R1-R3 R2-R3 R2-R4 R3-R4 R3-R5 R4-R5 R4-R6 R5-R6)
-for net in R1-R2 R1-R3 R2-R3 R2-R4 R3-R4 R3-R5 R4-R5 R4-R6 R5-R6 ; do
-#for net in ${LAN[@]} ; do
+#for net in R1-R2 R1-R3 R2-R3 R2-R4 R3-R4 R3-R5 R4-R5 R4-R6 R5-R6 ; do
 #for net in ${LAN[*]} ; do
-    
+
+LAN="R1-R2 R1-R3 R2-R3 R2-R4 R3-R4 R3-R5 R4-R5 R4-R6 R5-R6"
+for net in $LAN ; do
     #echo "$net i=$i"
     echo "esxcfg-vswitch -A $net $SW"
     esxcfg-vswitch -A $net $SW
