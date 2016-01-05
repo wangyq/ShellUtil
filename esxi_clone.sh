fab3622a9c50faf0f8ebcdd6a199d0de3e4358a1
Yinqing Wang <yinqingwang@gmail.com>
2016-01-05 12:17:57 +0800

    add

--- esxi_clone.sh
+++ esxi_clone.sh
@@ -16,11 +16,22 @@ run()
 L=1
 H=9
 
+ORIG=WR1
+VM=WR
+
 #for i in $(seq 2 8)
 for i in $(seq $L $H)
     do
         #ssh root@ESXi127 "cp /vmfs/volumes/datastore1/template/archlinux32/* /vmfs/volumes/datastore8/R${i}/"
         echo $i
+        
+        echo $VM$i
+        #mkdir $VM$i
+        echo $ORIG/archlinux32.vmx $VM$i/
+        #cp $ORIG/archlinux32.vmx $VM$i/
+        echo -i ./$ORIG/archlinux32.vmdk -d thin ./$VM$i/archlinux32.vmdk
+        #vmkfstools -i ./$ORIG/archlinux32.vmdk -d thin ./$VM$i/archlinux32.vmdk
+
         #mkdir WR$i
         #cp WR1/archlinux32.vmx WR$i/
         #vmkfstools -i ./WR1/archlinux32.vmdk -d thin ./WR$i/archlinux32.vmdk
