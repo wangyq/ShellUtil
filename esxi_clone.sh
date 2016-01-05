0a034c6f7c88e28fdfd860e734ddb76af6bf11b2
Yinqing Wang <yinqingwang@gmail.com>
2016-01-05 15:30:46 +0800

    fine for esxi cmd

--- esxi_clone.sh
+++ esxi_clone.sh
@@ -1,36 +1,95 @@
 #!/bin/sh
 
-#for i in $(seq 2 4)
-#    do
-        #ssh root@ESXi127 "cp /vmfs/volumes/datastore1/template/archlinux32/* /vmfs/volumes/datastore8/R${i}/"
-#        echo $i
-        #mkdir PC-$i
-        #cp ./template/winxp/WinXP.vmx PC-$i/WinXP.vmx
-        #vmkfstools -i /vmfs/volumes/datastore1/template/winxp/WinXP.vmdk -d thin /vmfs/volumes/datastore1/PC-$i/WinXP.vmdk
-#    done
+#----- Begin author and version ------
+# author: siwind (yinqingwang@gmail.com)
+# date: Dec 30, 2015
+# version: 1.0
+#
+#----- End of version -----
 
+
+# when paste, just using ":set noautoindent"
+
+#====================================
+#
+#parameter: vmpath vmname [DEBUG] 
+register_vm()
+{
+    vmpath=$1
+    vmname=$2
+    DEBUG=$3
+    
+    if [ -n "$DEBUG" ]; then
+        echo "Path=$vmpath, Name=$vmname"
+        echo "vim-cmd solo/registervm $vmpath $vmname"
+    else
+        #vim-cmd solo/registervm [path] [name]
+        vim-cmd solo/registervm $vmpath $vmname
+    fi
+
+}
+
+#==== need clone vmx file and vmdk file both ==
+# parameter: srcpath dstpath vmname [DEBUG]
+clone_vm()
+{
+    srcpath=$1
+    dstpath=$2
+    vmname=$3
+    DEBUG=$4
+    
+    if [ -n "$DEBUG" ]; then
+        echo "cp $srcpath/$vmname.vmx $dstpath/$vmname.vmx"
+        echo "vmkfstools -i $srcpath/$vmname.vmdk -d thin $dstpath/$vmname.vmdk"
+    else
+        cp $srcpath/$vmname.vmx $dstpath/$vmname.vmx
+        vmkfstools -i $srcpath/$vmname.vmdk -d thin $dstpath/$vmname.vmdk
+    fi
+
+}
 
 run()
 {
+DEBUG=$1
+
 #low and hight of index
-L=1
+L=2
 H=9
 
-ORIG=WR1
-VM=WR
+#vm name to copy by
+VMNAME=archlinux32
+SRCDIR=./WR1
+
+#destiname vm prefix
+DSTPREF=WR
+
+DSTDIR=.
 
 #for i in $(seq 2 8)
 for i in $(seq $L $H)
     do
+        dstname=${DSTPREF}$i
+        dst=${DSTDIR}/$dstname
+
+        if [ "$dst"x = "$SRCDIR"x ]; then
+            echo "Source and Destination vm is the same. DIR=$dst"
+            exit
+        fi
+
+        clone_vm $SRCDIR $dst $VMNAME $DEBUG
+
+        register_vm $dst/$VMNAME.vmx $dstname $DEBUG
+
         #ssh root@ESXi127 "cp /vmfs/volumes/datastore1/template/archlinux32/* /vmfs/volumes/datastore8/R${i}/"
-        echo $i
+        #echo $i
         
-        echo $VM$i
+        #echo $VM$i
         #mkdir $VM$i
-        echo $ORIG/archlinux32.vmx $VM$i/
+        #echo $ORIG/archlinux32.vmx $VM$i/
         #cp $ORIG/archlinux32.vmx $VM$i/
-        echo -i ./$ORIG/archlinux32.vmdk -d thin ./$VM$i/archlinux32.vmdk
+        #echo -i ./$ORIG/archlinux32.vmdk -d thin ./$VM$i/archlinux32.vmdk
         #vmkfstools -i ./$ORIG/archlinux32.vmdk -d thin ./$VM$i/archlinux32.vmdk
+
 
         #mkdir WR$i
         #cp WR1/archlinux32.vmx WR$i/
@@ -53,8 +112,10 @@ main()
 case $1 in
     run)
         run ;;
+    test)
+        run test;;
     *)
-    echo "Usage: $0 run"
+    echo "Usage: $0 run | test"
 esac
 }
 
--- esxi_cmd.sh
+++ esxi_cmd.sh
@@ -1,32 +1,46 @@
 #!/bin/sh
+#----- Begin author and version ------
+# author: siwind (yinqingwang@gmail.com)
+# date: Dec 30, 2015
+# version: 1.0
+#
+#----- End of version -----
 
-#esxcfg-vswitch -l
-#esxcfg-vswitch -A pg_name -v vid vSwitch3
-
+# paste using ":set noautoindent"
 
 run()
 {
+DEBUG=$1
+
 SW=vSwitch3
 
-i=15
+STARTVID=15
+
+
 #LAN=(R1-R2 R1-R3 R2-R3 R2-R4 R3-R4 R3-R5 R4-R5 R4-R6 R5-R6)
 #for net in R1-R2 R1-R3 R2-R3 R2-R4 R3-R4 R3-R5 R4-R5 R4-R6 R5-R6 ; do
 #for net in ${LAN[*]} ; do
 
+i=$STARTVID
 LAN="R1-R2 R1-R3 R2-R3 R2-R4 R3-R4 R3-R5 R4-R5 R4-R6 R5-R6"
 for net in $LAN ; do
-    #echo "$net i=$i"
-    echo "esxcfg-vswitch -A $net $SW"
-    esxcfg-vswitch -A $net $SW
-    echo "esxcfg-vswitch -p $net -v $i $SW"
-    esxcfg-vswitch -p $net -v $i $SW
-    #esxcfg-vswitch -A $net -v $i $SW
+    if [ -n "$DEBUG" ] ; then
+        echo "esxcfg-vswitch -A $net $SW"
+        echo "esxcfg-vswitch -p $net -v $i $SW"
+    else
+        esxcfg-vswitch -A $net $SW
+        esxcfg-vswitch -p $net -v $i $SW
+
+    fi
+    #esxcfg-vswitch -A $net -v $i $SW  (error: must one opertation a time)
     #i=`expr $i+1`
     #((i++))
     i=$(($i + 1))
 done
 
-esxcfg-vswitch -l
+if [ -z "$DEBUG" ] ; then
+    esxcfg-vswitch -l
+fi
 
 }
 
@@ -41,8 +55,10 @@ main()
 case $1 in
     run)
         run ;;
+    test)
+        run test ;;
     *)
-    echo "Usage: $0 run"
+    echo "Usage: $0 run | test"
 esac
 }
 
