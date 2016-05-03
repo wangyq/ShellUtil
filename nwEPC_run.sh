18a8edf941219615a89306c69c97d79868f6de65
Yinqing Wang <yinqingwang@gmail.com>
2016-05-03 10:04:54 +0800

    modify nwEPC_run.sh

--- nwEPC_run.sh
+++ nwEPC_run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-NWEPC_PATH=~/bin/nwepc-0.16
+#NWEPC_PATH=~/bin/nwepc-0.16
+NWEPC_PATH=i~/bin/nwepc-0.17
 APP_MME=$NWEPC_PATH/nw-mme-emu/nwLteMmeEmu
 APP_GW=$NWEPC_PATH/nw-sae-gw/nwLteSaeGw
 
@@ -10,14 +11,17 @@ APP_GW=$NWEPC_PATH/nw-sae-gw/nwLteSaeGw
 
 mme()
 {
-    #sudo $APP_MME --mme-ip 10.0.0.1 --sgw-ip 10.0.0.6 --pgw-ip 10.0.0.8 --gtpu-ip 10.0.0.1 --num-of-ue 10 --tun-if eth2
-    sudo $APP_MME --mme-ip 10.0.0.1 --sgw-ip 10.0.0.6 --pgw-ip 10.0.0.8 --gtpu-ip 10.0.0.1 --tun-if eth2
+    $APP_MME --mme-ip 10.0.0.1 --sgw-ip 10.0.0.6 --pgw-ip 10.0.0.8 --gtpu-ip 10.0.0.1 --num-of-ue 1 --tun-if eth2 
+    #$APP_MME --mme-ip 10.0.0.1 --sgw-ip 10.0.0.6 --pgw-ip 10.0.0.8 --gtpu-ip 10.0.0.1 --num-of-ue 1 --tun-if eth2 --session-timeout 120 --reg-per-sec 100
+    #$APP_MME --mme-ip 10.0.0.1 --sgw-ip 10.0.0.6 --pgw-ip 10.0.0.8 --gtpu-ip 10.0.0.1 --num-of-ue 2 --session-timeout 120 --reg-per-sec 100
+
     echo "Run nwEPC's MME finished!"
 }
 
 sgwpgw()
 {
-    sudo $APP_GW --sgw-s11-ip 10.0.0.6 --sgw-s5-ip 10.0.0.7 --pgw-s5-ip 10.0.0.8 --gtpu-ip 10.0.0.6 --apn lte5g --ippool-subnet 10.10.10.0 --ippool-mask 255.255.255.0 --sgi-if eth2 -cgw
+    $APP_GW --sgw-s11-ip 10.0.0.6 --sgw-s5-ip 10.0.0.7 --pgw-s5-ip 10.0.0.8 --gtpu-ip 10.0.0.6 --apn lte5g --ippool-subnet 192.168.10.0 --ippool-mask 255.255.255.0 --sgi-if eth2 -cgw
+    #$APP_GW --sgw-s11-ip 10.0.0.6 --sgw-s5-ip 10.0.0.7 --pgw-s5-ip 10.0.0.8 --gtpu-ip 10.0.0.6 --apn 3gpp --ippool-subnet 192.168.10.0 --ippool-mask 255.255.255.0 -cgw
 
     echo "Run Sgw/Pgw finished!"
 }
