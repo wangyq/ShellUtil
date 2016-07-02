cdbc3b8e240e35bac10f666b5cd938387411d161
Yinqing Wang <yinqingwang@gmail.com>
2016-07-02 20:49:26 +0800

    add ofsoftswitch run.sh

---
+++ ofsoftswitch_run.sh
@@ -0,0 +1,56 @@
+#! /bin/sh
+
+SWITCHID=00000000000C
+INTERFACES=eth1,eth2
+
+PTCP=6653
+
+#switch ip and controller ip:port to connect
+SWITCHIP=192.168.100.190
+CONTROLLERIP=192.168.100.155
+CPTCP=6633
+
+start()
+{
+	SWID=$1
+	if [ -z "$SWID" ] ; then
+		SWID=$SWITCHID
+	fi
+	echo "Begin create ofswitch=$SWID with interfaces=$INTERFACES, ptcp=$PTCP ......"
+	sudo ofdatapath --datapath-id=$SWID --interfaces=$INTERFACES ptcp:$PTCP --no-slicing
+	echo "Done."
+}
+
+conn()
+{
+	CPIP=$1
+	if [ -z "$CPIP" ] ; then
+		CPIP=$CONTROLLERIP
+	fi
+	echo "Begin connect to controller=$CPIP..."
+	ofprotocol tcp:$SWITCHIP: tcp:$CPIP:$CPTCP
+}
+
+run_cmd()
+{
+	echo "run command done."
+}
+
+#==== Here is the entry of script ====
+main()
+{
+case $1 in
+        start)
+                start $2 ;;
+        run)
+                run_cmd ;;
+        conn)
+                conn $2 ;;
+        show)
+                show ;;
+        *)
+        echo "Usage: $0 start <id=12> | conn <controller-ip> | show "
+esac
+}
+
+main $@
