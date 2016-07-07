f20b8b7942add4994da5aa1aa4e254b72d34f7dd
Yinqing Wang <yinqingwang@gmail.com>
2016-07-07 08:14:05 +0800

    ok

--- batch_cmd_run.sh
+++ batch_cmd_run.sh
@@ -26,10 +26,16 @@ HIGH=9
 
 #ARR=(AS1-1 AS2-1 AS2-2 AS2-3 AS3-1 AS4-1 AS4-2 AS5-1 AS5-2 AS5-3 AS6-1 AS6-2 AS7-1 AS7-2 AS8-1 AS8-2)
 #for ele in ${ARR[*]} ; do
-RRS="R1 R2 R3 R4 R5 R6"
+RRS="R1 R2 R3 R4 R5 R6 R7 R8"
+#RRS="R8"
 for h in $RRS ; do
     echo $h
-    ssh $h "hostname;"
+    #ssh-copy-id root@$h
+    #echo "Archlinux-$h" > tmpfile
+    #scp tmpfile root@$h:/etc/hostname
+    #ssh root@$h "hostname;reboot;exit"
+    #ssh root@$h "hostname;cat /etc/hostname;exit"
+    
 done
 
 }
