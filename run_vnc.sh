503e96e7e7009b4842d9679bc421dd9462aea0b5
Yinqing Wang <yinqingwang@gmail.com>
2016-11-12 16:43:27 +0800

    vnc

--- run_vnc.sh
+++ run_vnc.sh
@@ -1,31 +1,72 @@
 #!/bin/sh
 
-VNC_PATH=/opt/TigerVNC/bin
+#VNC_PATH=/opt/TigerVNC/bin
 #VNC_EXEC=$VNC_PATH/vncserver
 VNC_EXEC=vncserver
 SCREEN=1440x900
 #SCREEN=1260x700
-PORT=2
+DEFAULT_PORT=2
 
-if [ $# -eq 0 ] ; then
-        echo "Usage: $0 start | stop | restart | init"
-        exit 
-fi
+#====================================================================
+# Usage: run_vnc <START/STOP>  <only-stop> <start-port> <end-port>
+#
+run_vnc()
+{
+	START=$1  # whether start
 
-if [ $1 = "start" ] ; then
-        $VNC_EXEC -kill :$PORT
-        $VNC_EXEC :$PORT -geometry $SCREEN
-	echo "Start done. Please connect to the VM using vnc-viewer:  \"<IP>:$PORT\" "
-	echo ""
+	num=$#
+	#echo "num=$#"	
+	if [ "$num" -ge "3" ] ; then
+		sp=$2
+		ep=$3
+	elif [ "$num" -eq "2" ]; then
+		sp=$2
+		ep=$2
+	else
+		sp=$DEFAULT_PORT
+		ep=$DEFAULT_PORT
+	fi
 
-elif [ $1 = "stop" ] ; then
-        $VNC_EXEC -kill :$PORT
+	echo "start-port=$sp, end-port=$ep"
+	
+	for port in $(seq $sp $ep); do  #range is [sp,ep]
+		$VNC_EXEC -kill :$port   # first stop now!
+		if [ "$START" = "START" ] ; then
+			$VNC_EXEC :$port -geometry $SCREEN
+		fi
+	done
+}
 
-elif [ $1 = "restart" ] ; then
-        $VNC_EXEC -kill :$PORT
-        $VNC_EXEC :$PORT -geometry $SCREEN
-elif [ $1 = "init" ] ; then
-	vncpasswd
-else
-        echo Unkown paramter: "$1".   
-fi
+show()
+{
+	var=`ps -ef | grep vnc | grep desktop`
+	echo "VNC Server is:"
+	echo "$var"
+}
+
+#----------------------------------------------------#
+# shell entry to exec!
+#
+#----------------------------------------------------#
+main()
+{
+
+case $1 in
+        run)
+                run ;;
+        start)
+			run_vnc "START" $2 $3 ;;
+        stop)
+            run_vnc "STOP"  $2 $3 ;;
+		show)
+			show ;;
+        *)
+        echo "Usage: $0 start <start-port> <end-port> | stop <start-port> <end-port> | show"
+		echo "       where port number is from 1 to 20."
+		echo "       default port is 2"
+esac
+}
+
+# shell entry to start
+main $@
+
