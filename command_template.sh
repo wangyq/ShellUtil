ef65328cd46ed2663221f21f8a17bda717e15a6d
Yinqing Wang <yinqingwang@gmail.com>
2016-11-17 15:15:22 +0800

    ok

--- command_template.sh
+++ command_template.sh
@@ -6,11 +6,34 @@
 #
 function random()
 {
+MAXVALUE=2147483647
     min=$1
-    max=$(($2-$1+1))   #range
-    num=$(date +%s%N)  #too long to calculate!
-    echo $((num%max+min))   #进行求余数运算即可
+    max=$2 
+    if [ $MAXVALUE -lt $max ]; then
+        echo "Error: number is out of rang! $max"
+        exit
+    fi
+
+    base=$((max-min+1))   # range, caution may be MAXVALUE+1
+
+    if [ $base -le 0 -o $base -gt $MAXVALUE ]; then 
+        echo "Error: range=[$min $max]"
+        exit
+    fi
+
+    #num=$(date +%s%N)  #too long to calculate!
+    #num=`od -An -N4 -i /dev/urandom`  # 4-byte value
+    if [ $max -lt 256 ]; then num=`od -An -N1 -i /dev/urandom`
+    elif [ $max -lt 65536 ]; then num=`od -An -N2 -i /dev/urandom`
+    elif [ $max -lt 16777216 ]; then num=`od -An -N3 -i /dev/urandom`
+    else
+        num=`od -An -N2 -i /dev/urandom`
+        if [ 0 -gt $num ]; then num=$((-num)); fi
+    fi
+
+    echo $((num%base+min))   #进行求余数运算即可
 }
+
 
 #----------------------------------------------------#
 # get process'id from process name using grep command
