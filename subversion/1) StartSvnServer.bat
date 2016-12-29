e0ffd5de961fbe1b9c718b05363e8ab74ae05a36
Yinqing Wang <yinqingwang@gmail.com>
2016-12-29 11:17:52 +0800

    subversion

---
+++ subversion/1) StartSvnServer.bat
@@ -0,0 +1,59 @@
+@echo off
+color 0A
+
+set SVNDIR=svn-win32-1.8.17
+
+set CUR_DIR=%~dp0
+
+::取盘符信息
+SET DRIVER_DIR=%~d0
+
+REM 为了防止目录有空格，加了引号
+set EXECUTABLE="%CUR_DIR%%SVNDIR%\bin\svnserve.exe"
+
+REM 请设置此目录为你的 仓储目录
+REM 注意先用命令: "svnadmin.exe  create 你的仓储目录" 创建之
+REM set REPOS_DIR=L:/svn_data/svn_repos
+REM set /P REPOS_DIR=请输入SVN存储的路径(例如：L:/svn_data/svn_repos) :
+
+::set /P REPOS_DRIVE=请输入SVN存储的盘符(例如：C  D  E  F ...等) : 
+::set /P REPOS_RELPATH=请输入SVN存储的路径(例如：svn_data/svn_repos) : 
+::IF "%REPOS_RELPATH%"=="" SET REPOS_RELPATH=svn_data/svn_repos
+
+::SET REPOS_DIR=%REPOS_DRIVE%:/%REPOS_RELPATH%
+::SET REPOS_DIR=%DRIVER_DIR%\svn_data\svn_repos
+
+::SET REPOS_DIR=%CUR_DIR%svn_data\svn_repos
+
+SET REPOS_DIR=%CUR_DIR%svn_repos
+if not exist  %REPOS_DIR%/nul (
+  ECHO 目录%REPOS_DIR%不存在! 请检查输入!!!
+  GOTO END
+  )
+ 
+ECHO 当前盘符: %DRIVER_DIR%
+ECHO 当前路径: %CUR_DIR%
+ECHO 可执行文件: %EXECUTABLE%
+ECHO SVN仓储路径: %REPOS_DIR%
+
+ECHO 请按 "CTRL+C" 结束SVN运行!
+ECHO 正在运行SVN服务器......
+ECHO 访问路径: "svn://localhost/仓库名(bupt/work)/"
+
+%EXECUTABLE% -d -r %REPOS_DIR%
+
+REM echo SVN仓储位置: %REPOS_DIR%
+REM echo 正在安装SVN服务器......
+REM sc create svn binpath= "%EXECUTABLE% --service -r %REPOS_DIR%" displayname= "Subversion Server" depend= Tcpip start= auto
+
+REM echo 正在启动SVN服务器......
+REM net start svn
+
+REM 添加自动删除服务
+REM ECHO =====================================
+REM SET /P MM_TEMP=请按任意键删除SVN服务......
+REM net stop svn
+REM sc delete svn
+
+REM :END
+echo . &pause
--- subversion/install.bat
+++ subversion/install.bat
@@ -1,45 +1,68 @@
 @echo off
 color 0A
 
+set SVNDIR=svn-win32-1.8.17
 
-set CUR_DIR=%CD%
+REM 后面会有个'/'
+set CUR_DIR=%~dp0
 
 
 ::取盘符信息
-CD \
-SET DRIVER_DIR=%CD%
-CD %CUR_DIR%
-
+SET DRIVER_DIR=%~d0
 
 REM 为了防止目录有空格，加了引号
-set EXECUTABLE="%CUR_DIR%\bin\svnserve.exe"
+set EXECUTABLE=%CUR_DIR%%SVNDIR%\bin\svnserve.exe
 
 REM 请设置此目录为你的 仓储目录
 REM 注意先用命令: "svnadmin.exe  create 你的仓储目录" 创建之
 REM set REPOS_DIR=L:/svn_data/svn_repos
 REM set /P REPOS_DIR=请输入SVN存储的路径(例如：L:/svn_data/svn_repos) :
-set /P REPOS_DRIVE=请输入SVN存储的盘符(例如：C  D  E  F ...等) : 
-set /P REPOS_RELPATH=请输入SVN存储的路径(例如：svn_data/svn_repos) : 
-IF "%REPOS_RELPATH%"=="" SET REPOS_RELPATH=svn_data/svn_repos
 
-SET REPOS_DIR=%REPOS_DRIVE%:/%REPOS_RELPATH%
+ECHO "先用命令: 'svnadmin.exe  create 你的仓储目录' 创建之"
+
+set REPOS_DRIVE=
+set /P REPOS_DRIVE=请输入SVN存储的盘符(例如：C  D  E  F ...等, 回车默认为当前盘符) : 
+IF "%REPOS_DRIVE%"=="" ( SET REPOS_DRIVE=%DRIVER_DIR%) ELSE ( SET REPOS_DRIVE=%REPOS_DRIVE%:)
+
+set REPOS_RELPATH=
+set /P REPOS_RELPATH=请输入SVN存储的路径(例如：Subversion/svn_repos) : 
+IF "%REPOS_RELPATH%"=="" SET REPOS_RELPATH=Subversion/svn_repos
+
+
+SET REPOS_DIR=%REPOS_DRIVE%/%REPOS_RELPATH%
 
 if not exist  %REPOS_DIR%/nul (
-  ECHO 目录%REPOS_DIR%不存在! 请检查输入!!!
+  ECHO 目录 "%REPOS_DIR%" 不存在! 请检查输入!!!
   GOTO END
   )
 
-echo SVN仓储位置: %REPOS_DIR%
+ECHO ============================
+ECHO 当前盘符: %DRIVER_DIR%
+ECHO 当前路径: %CUR_DIR%
+ECHO 可执行文件: %EXECUTABLE%
+ECHO SVN仓储路径: %REPOS_DIR%
+ECHO 访问路径: svn://localhost/仓库名(bupt/work)/
+ECHO .
+
+::ECHO 请按CTRL+C结束SVN运行!
+::ECHO 正在运行SVN服务器......
+::ECHO 访问路径: svn://localhost
+
+REM 直接运行
+::%EXECUTABLE% -d -r %REPOS_DIR%
+
+REM echo SVN仓储位置: %REPOS_DIR%
 echo 正在安装SVN服务器......
-sc create svn binpath= "%EXECUTABLE% --service -r %REPOS_DIR%" displayname= "Subversion Server" depend= Tcpip start= auto
+sc create svn binpath="%EXECUTABLE% --service -r %REPOS_DIR%" displayname= "Subversion Server" depend=Tcpip start=auto
 
 echo 正在启动SVN服务器......
 net start svn
 
 REM 添加自动删除服务
-ECHO =====================================================
-SET /P MM_TEMP=请按任意键删除SVN服务......
-CALL REMOVE.BAT
+REM ECHO =====================================
+::SET /P MM_TEMP=请按任意键删除SVN服务......
+::net stop svn
+::sc delete svn
 
-:END
+REM :END
 echo . &pause
