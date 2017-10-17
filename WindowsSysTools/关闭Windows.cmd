6e29d6de6cc2defe51b9346e4fa0ad92a69b4343
admin <admin@DESKTOP-O3M8TS2>
2017-10-17 20:35:39 +0800

    add windows sys tools

---
+++ WindowsSysTools/关闭Windows.cmd
@@ -0,0 +1,9 @@
+@ECHO OFF 
+COLOR 0A
+
+::3秒后重启
+@ECHO 3秒后关闭Windows!
+SHUTDOWN /t 3 /s
+
+:: Pause
+PAUSE
---
+++ WindowsSysTools/重启Windows.cmd
@@ -0,0 +1,9 @@
+@ECHO OFF 
+COLOR 0A
+
+::3秒后重启
+@ECHO 3秒后重启!
+SHUTDOWN /t 3 /r
+
+:: Pause
+PAUSE
