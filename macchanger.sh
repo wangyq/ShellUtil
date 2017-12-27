00521885335e42509345f9b83b944597e94bc658
wangyq <yinqingwang@gmail.com>
2017-12-27 16:48:22 +0800

    macchanger

--- macchanger.sh
+++ macchanger.sh
@@ -4,6 +4,10 @@
 # /etc/netctl/interfaces/[your-interface]
 # /usr/bin/macchanger -r interface 
 
+#for vm mac-address
+#macaddr=$(echo 00:0c$(od -txC -An -N4 /dev/random|tr \  :))
+
+#macaddr=$(echo -n 02; od -t x1 -An -N 5 /dev/urandom | tr ' ' ':')
 #macaddr=$(echo $FQDN|md5sum|sed 's/^\(..\)\(..\)\(..\)\(..\)\(..\).*$/02:\1:\2:\3:\4:\5/')
 #macaddr=$(dd if=/dev/urandom bs=1024 count=1 2>/dev/null|md5sum|sed 's/^\(..\)\(..\)\(..\)\(..\)\(..\)\(..\).*$/\1:\2:\3:\4:\5:\6/')
 
@@ -13,8 +17,7 @@ run()
 INTFS="ens34 "
 
 for f in $INTFS ; do
-    macaddr=$(dd if=/dev/urandom bs=1024 count=1 2>/dev/null|md5sum|sed 's/^\(..\)\(..\)\(..\)\(..\)\(..\)\(..\).*$/\1:\2:\3:\4:\5:\6/')
-
+	macaddr=$(echo 00:0c$(od -txC -An -N4 /dev/random|tr \  :))
     echo "interfac:  $f macaddr = $macaddr"
     ip link set dev $f down
     ip link set dev $f address $macaddr
@@ -32,12 +35,12 @@ changemac()
 	if [ -z "$INTF" ] ; then
 		return
 	fi
-
-    macaddr=$(dd if=/dev/urandom bs=1024 count=1 2>/dev/null|md5sum|sed 's/^\(..\)\(..\)\(..\)\(..\)\(..\)\(..\).*$/\1:\2:\3:\4:\5:\6/')
-
+	macaddr=$(echo 00:0c$(od -txC -An -N4 /dev/random|tr \  :))
 	ip link set dev $INTF down
 	ip link set dev $INTF address $macaddr
 	ip link set dev $INTF up
+	dhclient -r $INTF
+	dhclient -1 $INTF
 	ip link show $INTF
 }
 
@@ -50,7 +53,11 @@ usage()
     #echo "$# parameters"    
     echo "Usage: "
     echo "     $0 INTF1 INTF2 ..."
-    echo "        where INTF1,INTF2 is the network interface name, such as eth0, enp3s0, etc."	
+    echo "        where INTF1,INTF2 is the network interface name, such as eth0, enp3s0, etc."
+	echo " "
+	echo "         after that , run command to renew ip-address:"
+	echo "                             sudo dhclient -r *interface*/sudo dhclient -1 *interface*"
+	echo "                             dhcpcd -k interface/dhcpcd interface"
 }
 
 #----------------------------------------------------#
@@ -61,6 +68,7 @@ main()
 {
 	if [ $# = 0 ] ; then
 		usage $@;
+		#run 	;
 		return ;
 	fi
 	for f in $*;
