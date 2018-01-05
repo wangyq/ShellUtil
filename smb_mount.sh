bc1c7c6ef0f8b19661926eb2d225556121a2209f
wangyq <yinqingwang@gmail.com>
2018-01-05 16:26:10 +0800

    smb_mount.sh

--- smb_mount.sh
+++ smb_mount.sh
@@ -14,10 +14,14 @@ start()
     #sudo mount //192.168.6.84/gg /mnt/share/ -o iocharset=utf8,username=administrator,password=123,dir_mode=0777,file_mode=0777,
 codepage=cp936,uid=0
 
-    #sudo mount -t cifs -o username=user,password=123456789,uid=xx,gid=xx $SMB_SERVER $MOUNT_DIR
-    #sudo mount -t cifs -o username=user,password=123456789,uid=$UID,gid=$GID $SMB_SERVER $MOUNT_DIR
-    
-    sudo mount -t cifs -o vers=1.0,username=user,password=123456789,uid=$UID,gid=$GID $SMB_SERVER $MOUNT_DIR
+    #sudo mount -t cifs -o username=user,password=xxx,uid=xx,gid=xx $SMB_SERVER $MOUNT_DIR
+    #sudo mount -t cifs -o username=user,password=xxx,uid=$UID,gid=$GID $SMB_SERVER $MOUNT_DIR
+
+    # Opened case with redhat - 
+    # They said that if you are running RHEL 4,5,6 - 
+    # you can only use SMBV1 protocol - You would need to upgrade to RHEL7 to use SMBv2 or 3 
+
+    sudo mount -t cifs -o vers=1.0,username=user,password=xxx,uid=$UID,gid=$GID $SMB_SERVER $MOUNT_DIR
 }
 
 stop()
