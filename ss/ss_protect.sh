d752a8b8ffbe11f8af543582c3643be6a7cd1c28
siwind <yinqingwang@163.com>
2020-02-27 16:54:43 +0800

    ss protect.sh

---
+++ ss/ss_protect.sh
@@ -0,0 +1,107 @@
+#!/bin/sh
+
+DEBUG=true
+
+LOGCMD="journalctl --no-pager --since today -u ss-server"
+
+RESALT="repeat salt detected"
+FAILSHAKE="failed to handshake with "
+FAILSHAKE_END=": authentication error"
+
+FWRELOAD="firewall-cmd --reload	"
+FWLISTRULE="firewall-cmd --list-rich-rules "
+
+PROTOCOL=tcp
+PORT=44444
+ACTION=reject
+
+PROTOCOL1=udp
+PORT1=44444
+ACTION1=reject
+
+#----------------------------------------------------#
+# return substring's index (start from 0)
+# otherwise, return -1
+#
+#----------------------------------------------------#
+strindex() {
+	x="${1%%$2*}"
+	[[ "$x" = "$1" ]] && echo -1 || echo "${#x}"
+}
+
+is_ipv6_str() {
+	local str=${1^^} #to upper case
+	local len=${#str}
+	if [ $len -gt 15 ]; then echo true; fi
+	if [[ $str = "*A*" ]] || [[ $str = "*B*" ]] || [[ $str = "*C*" ]] || [[ $str = "*D*" ]] || [[ $str = "*E*" ]] || [[ $str = "*F*" ]] || [[ $str = "*::*" ]]; then echo true; fi
+
+	echo false
+}
+
+#----------------------------------------------------#
+# run app program
+#
+#----------------------------------------------------#
+run() {
+	#strs=$(cat a.txt)
+	strs=`$LOGCMD`
+
+	local resalt=false
+
+	while read -r line; do
+		if ! $resalt; then
+			if [[ $line =~ $RESALT ]]; then
+				#echo "repeat salt found: $line"
+				resalt=true
+			fi
+		elif [[ $line =~ $FAILSHAKE ]]; then
+			afirst=$(strindex "$line" "$FAILSHAKE")
+			last=$(strindex "$line" "${FAILSHAKE_END}")
+
+			alen=${#FAILSHAKE}
+			first=$((afirst + alen))
+			if [ $first -le $last ]; then
+				len=$((last - first))
+				ipstr=${line:first:len}
+				isIPv6=$(is_ipv6_str $ipstr)
+
+				if $isIPv6; then family="ipv6"; else family="ipv4"; fi
+				if ! $DEBUG; then
+					firewall-cmd --zone=public --add-rich-rule="rule family=$family source address=$ipstr port protocol=$PROTOCOL port=$PORT $ACTION"
+					firewall-cmd --zone=public --add-rich-rule="rule family=$family source address=$ipstr port protocol=$PROTOCOL1 port=$PORT1 $ACTION1"
+				else
+					echo "firewall-cmd --zone=public --add-rich-rule='rule family=$family source address=$ipstr port protocol=$PROTOCOL port=$PORT $ACTION' "
+				fi
+			fi
+		elif [[ $line =~ $RESALT ]]; then
+			resalt=true
+		else
+			resalt=false
+		fi
+
+	done <<<"$strs"
+
+	echo "run finished!"
+}
+
+#----------------------------------------------------#
+# shell entry to exec!
+#
+#----------------------------------------------------#
+main() {
+
+	case $1 in
+	run)
+		run
+		;;
+	start)
+		start
+		;;
+	*)
+		echo "Usage: $0 start | run"
+		;;
+	esac
+}
+
+# shell entry to start
+main $@
---
+++ util/util.sh
@@ -0,0 +1,142 @@
+#!/bin/sh
+
+# All signal's name and number can be found using command: "kill -l"
+S_TERM=15   #SIGTERM=15, SIGKILL=9
+S_RELOAD=1  #SIGHUP=1
+
+APP_PATH=/usr/local/bin
+APP_NAME=transmission-daemon
+APP_EXEC=$APP_PATH/$APP_NAME
+
+#----------------------------------------------------#
+# return substring's index (start from 0)
+# otherwise, return -1
+#
+#----------------------------------------------------#
+strindex() { 
+	x="${1%%$2*}"
+  	[[ "$x" = "$1" ]] && echo -1 || echo "${#x}"
+}
+
+is_ipv6_str(){
+	local str=${1^^}   #to upper case
+	local len=${#str}
+	if [ $len -gt 15 ] ; then  echo true; fi
+	if [[ $str = "*A*" ]] || [[ $str = "*B*" ]] || [[ $str = "*C*" ]] || [[ $str = "*D*" ]] || [[ $str = "*E*" ]] || [[ $str = "*F*" ]] || [[ $str = "*::*" ]] ; then echo true; fi
+
+	echo false
+}
+
+#----------------------------------------------------#
+# get process'id from process name using grep command
+# 
+#
+#----------------------------------------------------#
+get_pid()
+{
+	local program=$1
+        local pid=`ps -ef | grep "$program" | grep -v grep | awk  '{print   \$2}'`
+	echo $pid
+}
+
+#----------------------------------------------------#
+# run app program
+#
+#----------------------------------------------------#
+run()
+{
+    APP="hello goodday bye"
+    for app in $APP; do
+        echo $app
+    done
+
+        #echo "run ......"
+        # nohup /bin/sh -c "command_file.sh" > /dev/null 2>&1 &
+
+        $APP_EXEC
+
+        echo "run finished!"
+}
+
+#----------------------------------------------------#
+# start app program as daemon
+#
+#----------------------------------------------------#
+start()
+{
+	#echo "start ......"
+	nohup /bin/sh -c "$APP_EXEC" > /dev/null 2>&1 &
+
+	#$APP_EXEC
+	echo "start finished!"
+}
+
+#----------------------------------------------------#
+#
+#----------------------------------------------------#
+stop()
+{
+	#echo "stop ......"
+	local PID=`get_pid $APP_NAME`
+        #echo $APP_NAME\' pid = $PID !
+	if [ "$PID" != "" ];then
+		kill -s $S_TERM  $PID
+	else
+		true	
+		#echo $APP_NAME not found!
+	fi
+}
+
+#----------------------------------------------------#
+#
+#----------------------------------------------------#
+restart()
+{
+	stop
+	start 
+}
+
+#----------------------------------------------------#
+#
+#----------------------------------------------------#
+reload()
+{
+	#echo "reload ......"
+        local PID=`get_pid $APP_NAME`
+        #echo $APP_NAME\' pid = $PID !
+	if [ "$PID" != "" ];then
+        	kill -s $S_RELOAD  $PID
+		echo "reload finished!"
+	else
+		echo $APP_NAME not found!
+	fi
+}
+
+#----------------------------------------------------#
+# shell entry to exec!
+#
+#----------------------------------------------------#
+main()
+{
+
+case $1 in
+	run)
+		run ;;
+	start)
+		restart ;;
+	stop)
+		stop ;;
+	restart)
+		stop
+		start  ;;
+	reload)
+		reload ;;
+	*)
+        echo "Usage: $0 start | stop | restart | reload | run"
+esac
+}
+
+# shell entry to start
+main $@
+
+
