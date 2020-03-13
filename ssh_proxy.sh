9d4dbbf5d46d8e2cd45fdb73f9ada393e367a9e0
siwind <yinqingwang@gmail.com>
2020-03-13 11:45:23 +0800

    ssh_proxy.sh

--- ssh_proxy.sh
+++ ssh_proxy.sh
@@ -2,7 +2,7 @@
 
 # run in rc.local file
 #ssh -f -6 -N -D :2080 localhost
-#runuser -l user -s "/bin/sh"  -c "ssh -f -6 -N -D :2080 localhost"
+#runuser -l user -s "/bin/sh"  -c "nohup ssh -f -6 -N -D :2080 user@localhost >/dev/null 2>&1"
 
-ssh -f -6 -N -D :2080 localhost
+ssh -f -6 -N -D :2080 user@localhost >/dev/null 2>&1
 
